13f80238f9f6aca98c0ce06016a36b70d4cf2a79
ed <s@ocv.me>
2025-03-12 21:45:25 +0000

    efishell: fix directory colors in ls

--- uefi-shellbin/mkiso.sh
+++ uefi-shellbin/mkiso.sh
@@ -5,9 +5,9 @@ set -e
 # cp -pv /usr/share/OVMF/OVMF_{CODE,VARS}.fd . && qemu-system-x86_64 -cpu host -enable-kvm -cdrom rls/efishell.iso -drive file=OVMF_CODE.fd,format=raw,if=pflash -drive file=OVMF_VARS.fd,format=raw,if=pflash -m 512
 
 mkdir -p espdir/efi/boot
-cp -pv /rls/Shell.efi espdir/efi/boot/bootx64.efi
+cp -pv /rls/shell.efi espdir/efi/boot/bootx64.efi
 
-sz=$(wc -c </rls/Shell.efi | awk '{print int($1/1024)+256}')
+sz=$(wc -c </rls/shell.efi | awk '{print int($1/1024)+256}')
 touch esp
 truncate -s ${sz}k esp
 mkfs.vfat -F12 -nESP esp
--- uefi-shellbin/p2.sh
+++ uefi-shellbin/p2.sh
@@ -22,4 +22,4 @@ args=(
 build -t GCC5 -a X64 -b RELEASE -p ShellPkg/ShellPkg.dsc "${args[@]}"
 
 mkdir -p /rls
-cp -pv Build/Shell/RELEASE_GCC5/X64/ShellPkg/Application/Shell/EA4BB293-2D7F-4456-A681-1F22F42CD0BC/OUTPUT/Shell.efi /rls/
+cp -pv Build/Shell/RELEASE_GCC5/X64/ShellPkg/Application/Shell/EA4BB293-2D7F-4456-A681-1F22F42CD0BC/OUTPUT/Shell.efi /rls/shell.efi
