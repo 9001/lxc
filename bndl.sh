36e9624ce866f930af0145626e4fc0d0b8b90eb9
ed <s@ocv.me>
2026-03-01 16:54:57 +0000

    add pico2wave (another tts, sure why not)

--- bndl.sh
+++ bndl.sh
@@ -3,13 +3,15 @@ set -ex
 rm -rf bin bin*.sfx
 mkdir bin
 cp -pv */rls/* bin/ || true
+cp -pvR static-pico2wave/rls/pico2wave-voices bin/ || true
 #tar -cv bin | zstd --long -T0 -19 > bin.tzst
 bin/7z a -sfx -t7z -m0=lzma -mx=9 -mfb=64 -md=32m -ms=on bin-full.sfx bin
 
 (cd bin
-rm cmu_us_slt.*
+rm flite cmu_us_slt.*
 rm clzip lunzip lzd lzip plzip tarlz zcat zcmp zdiff zgrep ztest zupdate
 rm bsdcpio bsdunzip
+rm pico2wave-voices/{de,es,fr,it,en-US}*
 )
 bin/7z a -sfx -t7z -m0=lzma -mx=9 -mfb=64 -md=32m -ms=on bin.sfx bin
 
